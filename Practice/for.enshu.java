26431f1ece45334551711eff4541f65aa6b84f58
MT 中島 帆乃香 <honoka.nakajima@metateam.co.jp>
2024-05-01 14:30:56 +0900

    javatest1-1

---
+++ Practice/for.enshu.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("1～10の整数を入力してください。");
+          int i = new java.util.Scanner(System.in).nextInt();
+     
+     if(i < 5){
+         System.out.println("5未満の整数" + i + "が入力されました");
+     } else if(i == 5) {
+         System.out.println("5が入力されました");
+     } else {
+         System.out.println("6以上の整数" + i + "が入力されました");
+     }
+    }
+}
