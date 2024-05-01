31229a57c83087e8da21eebcb9e526804aac352d
MT 中島 帆乃香 <honoka.nakajima@metateam.co.jp>
2024-05-01 17:58:39 +0900

    javatest

---
+++ Practice/2-2.java
@@ -0,0 +1,16 @@
+public class Practice{
+    public static void main(String[] args){
+        
+
+        while(true) {
+            System.out.println("1～100の数字を入力してください");
+            int num = new java.util.Scanner(System.in).nextInt();
+        if(num % 7 == 0){
+           System.out.println("処理を終了しました。");
+           num++;
+           break;
+        } else {
+        }
+        }
+    }
+}
